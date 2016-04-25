71c37d2206f62ac0d791cbbcd8e6472d0209000c
Ben Baert <ben_b@gmx.com>
2016-04-25 14:23:18 +0200

    Added Codacy badge

--- irail.py
+++ irail.py
@@ -11,11 +11,20 @@ def parse_timestamp(seconds_str):
     return datetime.fromtimestamp(int(seconds_str)).strftime("%H:%M")
 
 
-def make_station_header(json_object, direction_filter, vehicle_filter):
+def make_station_header(json_object,
+                        direction_filter,
+                        vehicle_filter):
     name = json_object["stationinfo"]["standardname"]
     station_time = parse_timestamp(json_object["timestamp"])
-    msg = name + (" (direction: " + (direction_filter or vehicle_filter) + ")") if direction_filter or vehicle_filter else ""
-    click.secho(station_time + " " + str.center(msg, TERMINAL_WIDTH - 6), reverse = True)
+    msg = (name +
+            (" (direction: " +
+                (direction_filter or vehicle_filter) +
+                ")") if direction_filter or vehicle_filter
+            else "")
+    click.secho(station_time +
+                " " +
+                str.center(msg, TERMINAL_WIDTH - 6),
+                reverse=True)
     return name
 
 
@@ -29,24 +38,41 @@ def fetch_trains(json_object):
 
 
 def parse_vehicle(vehicle_id_string):
-    return ''.join(x for x in vehicle_id_string[8:10] if x in "PLIC").ljust(2)
+    return (''.join(x for x in vehicle_id_string[8:10]
+                    if x in "PLIC")
+              .ljust(2))
 
 
 def get_station(suggestion):
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    json_data = requests.get("https://irail.be/stations/NMBS/", {"q" : suggestion}, headers=headers).json()
+    headers = {'Content-type': 'application/json',
+               'Accept': 'text/plain'}
+    json_data = (requests.get(
+                    "https://irail.be/stations/NMBS/",
+                    {"q" : suggestion},
+                    headers=headers)
+                    .json())
     suggestions = json_data["@graph"]
 
     if len(suggestions) == 1:
         station_index = 0
             
     elif len(suggestions) == 0:
-        click.echo("No station like {0} found, please verify that you have an existing train station in Belgium and try again please.".format(suggestion))
+        click.echo(
+            """
+            No station like {0} found.
+            Please verify that you have
+            an existing train station in
+            Belgium and try again.
+            """.format(suggestion))
         raise SystemExit(0)
     else:
         for index, station in enumerate(suggestions):
             click.echo(str(index) + ": " + station["name"])
-        station_index = click.prompt('Which station do you mean by {0}?'.format(suggestion), type=int)
+        station_index = click.prompt(
+                            """
+                            Which station do you mean by {0}?
+                            """.format(suggestion),
+                            type=int)
 
     return suggestions[station_index]["name"]
 
@@ -60,22 +86,33 @@ def parse_delay(delay_str):
     else:
         delay = int(delay_str) // 60
         text = ("+" + str(delay)).ljust(3)
-        return False, click.style(text, fg = "red")
+        return False, click.style(text, fg="red")
 
 
 def parse_platform(train):
+    """
+    Extract platform from train object.
+    Apply style to platform string.
+    If platform is normal, simply return platform.
+    If platform has been changed, apply 'reverse' style.
+    """
     platform = train["platform"]
     platform_changed = train["platforminfo"]["normal"] != "1"
     platform_message = " " + platform.rjust(2)
     if platform_changed:
-        return click.style(platform_message, reverse = True)
+        return click.style(platform_message, reverse=True)
     return platform_message
 
 
 def vehicle_filter_check(train, vehicle_filter):
     if vehicle_filter:
         vehicle_id = train["vehicle"]
-        v = requests.get("http://api.irail.be/vehicle/", params = {"fast" : "true", "format" : "json", "id" : vehicle_id}).json()
+        v = requests.get(
+                "http://api.irail.be/vehicle/",
+                params={
+                    "fast": "true",
+                    "format": "json",
+                    "id" : vehicle_id}).json()
         stops = v["stops"]["stop"]
         current_station_passed = False
         for stop in stops:
@@ -249,7 +286,7 @@ def route(from_station_suggestion, to_station_suggestion, time, date, selection)
 
     connections = r["connection"]
 
-    optimal_connections = sorted(connections, key = lambda x: int(x["arrival"]["time"]) + int(x["duration"]) // 2)
+    optimal_connections = sorted(connections, key=lambda x: int(x["arrival"]["time"]) + int(x["duration"]) // 2)
     most_optimal_connection = optimal_connections.pop(0)
     optimal_departure_time, optimal_arrival_time, duration, changes = route_overview(most_optimal_connection)
     click.secho("Optimal connection: " + optimal_departure_time + "-->" + optimal_arrival_time + (duration + " " + changes).rjust(TERMINAL_WIDTH - 33), reverse = True, nl = False)
@@ -258,9 +295,11 @@ def route(from_station_suggestion, to_station_suggestion, time, date, selection)
     click.echo("Other options:")
     show_route_choices(optimal_connections)
     
-    v = click.confirm('Would you like to expand any of these?', abort = True)
+    v = click.confirm('Would you like to expand any of these?',
+                      abort=True)
     while v:
-        e = click.prompt("Which one (type 9 for all)?", type = int)
+        e = click.prompt("Which one (type 9 for all)?",
+                         type = int)
         if e == 9:
             for connection in optimal_connections:
                 expand_connection(connection)
@@ -277,16 +316,23 @@ def route(from_station_suggestion, to_station_suggestion, time, date, selection)
 @click.command()
 @click.argument('vehicle_id')
 def vehicle(vehicle_id):
-    r = requests.get("http://api.irail.be/vehicle/", params = {"id" : vehicle_id, "format" : "json", "fast" : "true"}).json()
-    click.secho(parse_timestamp(r["timestamp"]) + " " + str.center(r["vehicle"], TERMINAL_WIDTH - 6), reverse = True)
+    r = requests.get("http://api.irail.be/vehicle/", 
+                     params = {"id": vehicle_id,
+                               "format": "json", 
+                               "fast": "true"}).json()
+    click.secho(parse_timestamp(r["timestamp"]) +
+                " " +
+                str.center(r["vehicle"], TERMINAL_WIDTH - 6),
+                reverse = True)
     now = datetime.now()
     for stop in r["stops"]["stop"]:
         stop_time = parse_timestamp(stop["time"])
-        if (datetime.fromtimestamp(int(stop["time"])) + timedelta(seconds = int(stop["delay"]))) < now:
-            dim = True
-        else:
-            dim = False
-        click.secho(stop_time + parse_delay(stop["delay"])[1] + stop["station"], dim = dim)
+        dim = (datetime.fromtimestamp(int(stop["time"])) +
+               timedelta(seconds = int(stop["delay"]))) < now
+        click.secho(stop_time +
+                    parse_delay(stop["delay"])[1] +
+                    stop["station"],
+                    dim=dim)
 
 if __name__ == '__main__':
     route()
